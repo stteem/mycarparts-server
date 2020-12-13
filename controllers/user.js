e152bd68998068793da2e0f026ec26c942d0336a
Uwem Uke <slyart147@gmail.com>
2020-12-13 23:22:24 +0100

    verify google tokenId before authenticating user

--- controllers/user.js
+++ controllers/user.js
@@ -74,55 +74,74 @@ exports.login = (req, res, next) => {
     );
   }
 
+  const decodeToken = (token) => {
+    const decoded = jwt.decode(token, {complete: true});
+    return decoded;
+  }
+
   // Google sign in and sign up
   User.findOne({ email: req.body.email }).then(
     (user) => {
       console.log('lets try google')
       if (!user) {
-        console.log("not user, let's save user")
-        // No user was found, let's create a new one
-        const signUpGoogleUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          imageUrl: req.body.imageUrl
-        });
-        console.log('new g user ', signUpGoogleUser)
-        return signUpGoogleUser.save().then(
-          (response) => {
-            console.log('signUpGoogleUser res ', response)
-            console.log('user.objectId ', response._id)
-            const token = jwt.sign({ userId: response._id }, process.env.SECRET, { expiresIn: '24h' });
 
-            res.setHeader('Authorization', token);
-            console.log('Header set')
-              res.status(201).json({
-                token: token,
-                user: response.name,
-                email: response.email,
-                imageUrl: response.imageUrl,
-                message: 'User added successfully!'
-            });
-          }
-        ).catch(
-          (error) => {
-            res.status(500).json({
-              error: error
-            });
-          }
-        );
+        console.log("not user, let's save user")
+        // No user was found, let's create a new user
+        // but first let's verify that this is a google user
+        let decoded = decodeToken(req.body.token);
+
+        if (decoded.payload.iss == 'accounts.google.com') {
+          const signUpGoogleUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            imageUrl: req.body.imageUrl
+          });
+          console.log('new g user ', signUpGoogleUser)
+          return signUpGoogleUser.save().then(
+            (response) => {
+              console.log('signUpGoogleUser res ', response)
+              console.log('user.objectId ', response._id)
+              const token = jwt.sign({ userId: response._id }, process.env.SECRET, { expiresIn: '24h' });
+
+              res.setHeader('Authorization', token);
+              console.log('Header set')
+                res.status(201).json({
+                  token: token,
+                  user: response.name,
+                  email: response.email,
+                  imageUrl: response.imageUrl,
+                  message: 'User added successfully!'
+              });
+            }
+          ).catch(
+            (error) => {
+              res.status(500).json({
+                error: error
+              });
+            }
+          );
+        }
       }
 
-      // If we found the user
+      // If we found the user, let's verify that it's a google account
+      // and also compare the token email with the user email
       console.log('Oops! we already have that user, have him')
-      const token = jwt.sign({ userId: user._id }, process.env.SECRET, { expiresIn: '24h' });
-      res.setHeader('Authorization', token);
-      return res.status(200).json({
-                token: token,
-                user: user.name,
-                email: user.email,
-                imageUrl: user.imageUrl,
-                message: 'User found!'
-      });
+
+      let decoded = decodeToken(req.body.token);
+
+      if (decoded.payload.iss == 'accounts.google.com' && decoded.payload.email == user.email) {
+
+        const token = jwt.sign({ userId: user._id }, process.env.SECRET, { expiresIn: '24h' });
+
+        res.setHeader('Authorization', token);
+        return res.status(200).json({
+          token: token,
+          user: user.name,
+          email: user.email,
+          imageUrl: user.imageUrl,
+          message: 'User found!'
+        });
+      }
     }
   ).catch(
     (error) => {
